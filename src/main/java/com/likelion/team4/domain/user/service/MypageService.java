6c5263312f4452235653cd4e894229e5c619ce02
kjw <zw7977@gmail.com>
2026-08-11 02:44:14 +0900

    feat: 마이페이지 서비스 및 내 정보 조회 로직 구현

---
+++ src/main/java/com/likelion/team4/domain/user/service/MypageService.java
@@ -0,0 +1,29 @@
+package com.likelion.team4.domain.user.service;
+
+import com.likelion.team4.domain.user.dto.response.UserProfileResponse;
+import com.likelion.team4.domain.user.entity.User;
+import com.likelion.team4.domain.user.repository.UserRepository;
+import com.likelion.team4.global.exception.CustomException;
+import com.likelion.team4.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class MypageService {
+
+    private final UserRepository userRepository;
+
+    // 내 정보 조회
+    @Transactional(readOnly = true)
+    public UserProfileResponse getMyProfile(Long userId) {
+        User user = getUserById(userId);
+        return new UserProfileResponse(user);
+    }
+
+    private User getUserById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.RESOURCE_NOT_FOUND));
+    }
+}
