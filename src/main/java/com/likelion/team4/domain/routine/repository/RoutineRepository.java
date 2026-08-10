d962b4f902bf1b7c45c9540832c77df918001aa8
oyend <zw7977@gmail.com>
2026-08-11 00:31:25 +0900

    [BE-#21] 루틴 상세 조회 API 추가 및 유저 인증 방식(JWT) 적용 (#22)
    * feat: 삭제되지 않은 루틴을 단건 조회하는 메서드 추가
    
    * feat: 루틴 조회 후 정상 조회 시 반환하는 로직 추가
    
    * feat: 서비스를 호출하는 엔드포인트 추가
    
    * chore: 불필요한 코드 제거

--- src/main/java/com/likelion/team4/domain/routine/repository/RoutineRepository.java
+++ src/main/java/com/likelion/team4/domain/routine/repository/RoutineRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RoutineRepository extends JpaRepository<Routine, Long> {
     List<Routine> findAllByUser_IdAndDeletedAtIsNull(Long userId);
     List<Routine> findAllByUser_IdAndRepeatDaysContainingAndDeletedAtIsNull(Long userId, String day);
+    Optional<Routine> findByIdAndUser_IdAndDeletedAtIsNull(Long id, Long userId);
 }
