f410ed7b0788b1c32d0fdef3fa3e502a7e7a2ba0
kjw <zw7977@gmail.com>
2026-08-19 21:50:21 +0900

    fix: 닉네임, 비밀번호, 알림 요청 DTO 범위 제한 추가

--- src/main/java/com/likelion/team4/domain/user/dto/request/UpdateNicknameRequest.java
+++ src/main/java/com/likelion/team4/domain/user/dto/request/UpdateNicknameRequest.java
@@ -1,6 +1,7 @@
 package com.likelion.team4.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 public class UpdateNicknameRequest {
 
     @NotBlank(message = "닉네임은 필수 입력 값입니다.")
+    @Size(max = 50, message = "닉네임은 최대 50자까지 입력 가능합니다.")
     private String nickname;
 
     public UpdateNicknameRequest(String nickname) {
--- src/main/java/com/likelion/team4/domain/user/dto/request/UpdatePasswordRequest.java
+++ src/main/java/com/likelion/team4/domain/user/dto/request/UpdatePasswordRequest.java
@@ -13,7 +13,7 @@ public class UpdatePasswordRequest {
     private String currentPassword;
 
     @NotBlank(message = "새로운 비밀번호를 입력해주세요.")
-    @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
+    @Size(min = 8, max = 72, message = "비밀번호는 최소 8자 이상이어야 합니다.") // BCrypt 암호화 알고리즘의 72바이트 절삭 이슈 방지
     private String newPassword;
 
     public UpdatePasswordRequest(String currentPassword, String newPassword) {
