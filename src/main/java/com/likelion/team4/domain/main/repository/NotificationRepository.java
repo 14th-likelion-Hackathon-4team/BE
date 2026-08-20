1b7c79f671c963fddc6556bd750c6aa0ca40b412
dami6326 <dami6326@gmail.com>
2026-08-20 12:58:04 +0900

    feat: 시간대 수정 및 재알림 기능 추가

--- src/main/java/com/likelion/team4/domain/main/repository/NotificationRepository.java
+++ src/main/java/com/likelion/team4/domain/main/repository/NotificationRepository.java
@@ -2,9 +2,12 @@ package com.likelion.team4.domain.main.repository;
 
 import com.likelion.team4.domain.main.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface NotificationRepository
         extends JpaRepository<Notification, Long> {
@@ -25,6 +28,26 @@ public interface NotificationRepository
             LocalDateTime start,
             LocalDateTime end
     );
+
+    Optional<Notification> findTopByRoutine_IdOrderByCreatedAtDesc(Long routineId);
+
+    @Query("""
+        SELECT n
+        FROM Notification n
+        WHERE n.read = false
+        AND n.createdAt <= :time
+        """)
+    List<Notification> findUnreadNotificationsBefore(
+            @Param("time") LocalDateTime time
+    );
+
+    @Query("""
+        SELECT n
+        FROM Notification n
+        WHERE n.read = false
+        """)
+    List<Notification> findAllUnreadNotifications();
+
 
     void deleteAllByUser_Id(Long userId);
 }
